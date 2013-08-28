f86d3b8ed0d50235d2b759e5f036c47c4e047a2d
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-08-28 19:49:01 +0200

    Add more packages to install at start

--- scripts/start_packages.sh
+++ scripts/start_packages.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # Packages to be install at start
+
+# add repos
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+
+# Update, upgrade and install
 sudo apt-get update
-sudo apt-get install vim git-core curl
-# Sublime
-bash ./sublime/sublime_install.sh
+sudo apt-get -y upgrade
+sudo apt-get install vim vim-gnome git-core curl sublime-text
+
+# Sublime installation alternative
+# bash ./sublime/sublime_install.sh
 # n98-magerun
+
+
+# Update at the end
+sudo ap
